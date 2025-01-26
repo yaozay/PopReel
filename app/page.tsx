4de143a65d6e9c969730d430755c0b6e5f03c15f
yahya <yahyaozay1905@gmail.com>
2025-01-26 15:19:53 -0600

    last version

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -6,12 +8,12 @@ export default function Home() {
         Your platform for sharing and discovering amazing content.
       </p>
       <div className="mt-6">
-        <a
+        <Link
           href="/sign-up"
           className="px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
         >
           Get Started
-        </a>
+        </Link>
       </div>
     </div>
   );
